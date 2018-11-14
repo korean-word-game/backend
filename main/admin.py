906299f682ee8d3bc59634f2f4718be4529c704b
LMC <2_minchul@naver.com>
2018-11-15 04:12:53 +0900

    Optimize import statement

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import WordType,WordFile,Word,GameRoom
+
+from .models import WordType, Word, GameRoom
 
 admin.site.register(WordType)
 admin.site.register(Word)
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path,re_path
+from django.urls import path
+
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('db/', views.word_plus),
