2356fa9d1f83e3770ca1efab7add90038d48d4f6
LMC <2_minchul@naver.com>
2019-01-24 14:45:50 +0900

    channel config 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -124,4 +124,22 @@ STATICFILES_DIRS = [
     MEDIA_ROOT
 ]
 
+# channel configs
+ASGI_APPLICATION = 'config.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+
+            "hosts": [(
+                'redis://:{redis_pass}@{redis_host}:{redis_port}/{redis_db}'.format(
+                    redis_host='db.xn--2j1b940b.xn--lg3bt3ss6d.com',
+                    redis_pass='dmzteamitda',
+                    redis_port=6379,
+                    redis_db='1'
+                )
+            )],
+        },
+    },
+}
