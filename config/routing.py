8fd118c58b309fd4a1925ff399d84ab238ccad15
LMC <2_minchul@naver.com>
2019-01-24 14:47:04 +0900

    add routing.py

---
+++ config/routing.py
@@ -0,0 +1,13 @@
+# mysite/routing.py
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
