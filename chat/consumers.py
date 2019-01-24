8b3c8affa2e09ddbabba274ec2085e365189b1be
LMC <2_minchul@naver.com>
2019-01-24 19:02:05 +0900

    get_rooms 구현

--- chat/consumers.py
+++ chat/consumers.py
@@ -69,7 +69,52 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'user': self.user.username
         })
 
-    async def get_rooms(self):
+    async def event_get_rooms(self, event=None):
         models = Room.objects.filter(is_hide=False)
 
         print(serializers.serialize('json', models))
+
+
+class LobbyConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = None
+
+        token = self.scope['session'].get('token')
+        if token:
+            self.user = User.objects.get(token=token)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    # Receive message from WebSocket
+    async def receive(self, text_data=None, bytes_data=None):
+        if text_data:
+            text_data_json = json.loads(text_data)
+            print('receive data:', [text_data_json])
+            command_type = text_data_json.get('type')
+
+            if command_type == 'query':
+                if text_data_json.get('get_rooms'):
+                    await self.event_get_rooms(text_data_json)
+
+            # if command_type == 'chat_message':
+            #     message = text_data_json['message']
+            #     await self.message_received(message)
+
+            # Send message to room group
+
+    async def send_json(self, json_data):
+        print('send data:', json_data)
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps(json_data))
+
+    async def event_get_rooms(self, event=None):
+        models = Room.objects.filter(is_hide=False)
+        await self.send_json({
+            'type': 'query',
+            'query': 'get_rooms',
+            'callback': event['callback'],
+            'result': serializers.serialize('json', models)
+        })
--- chat/routing.py
+++ chat/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/', consumers.LobbyConsumer)
 ]
