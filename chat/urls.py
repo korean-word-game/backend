3596c522c5cb169e1be1144db9786e97089255fb
noname_Producer <nonamep765@gmail.com>
2019-01-24 16:02:24 +0900

    account chatting

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 # chat/urls.py
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    path('', views.index, name='index'),
+    path('<room_name>/', views.Room.as_view(), name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 
+from django.views import View
+
+from users.views import check_login
+from wordgame.utils import auto_login_controller
+
 
 def index(request):
     return render(request, 'chat/index.html', {})
@@ -13,3 +18,16 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+
+class Room(View):
+    rDict = dict(now='main')
+
+    def get(self, req, room_name):
+        check_login(req)
+        isLogin, user = auto_login_controller(req)
+        print(user)
+
+        rDict = dict(isLogin=isLogin, now='main', user=user)
+        rDict['room_name']=mark_safe(json.dumps(room_name))
+        return render(req, 'chat/room.html', rDict)
