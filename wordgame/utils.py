76528fdb0e1c3583414272c525bb85bc7e755b3a
noname_Producer <nonamep765@gmail.com>
2019-01-24 18:04:22 +0900

    add requirement

--- wordgame/utils.py
+++ wordgame/utils.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect
+from .models import Word, WordType
 
 from users.models import User
+import hgtk
 
 
 def auto_login_controller(req):
@@ -14,3 +16,19 @@ def auto_login_controller(req):
             req.session['isLogin'] = False
             return None, None
     return isLogin, user
+
+def search_word(type: int, word):
+    word_type = WordType.objects.get(id=type)
+
+def duu(p0, p1):
+    p0 = hgtk.letter.decompose(p0[0])
+    if p0[0] in ['ㄹ', 'ㄴ', 'ㅇ']:
+
+        if len(p0[2]) == 0:
+            p1.append(hgtk.letter.compose('ㄹ', p0[1]))
+            p1.append(hgtk.letter.compose('ㄴ', p0[1]))
+            p1.append(hgtk.letter.compose('ㅇ', p0[1]))
+        else:
+            p1.append(hgtk.letter.compose('ㄹ', p0[1], p0[2]))
+            p1.append(hgtk.letter.compose('ㄴ', p0[1], p0[2]))
+            p1.append(hgtk.letter.compose('ㅇ', p0[1], p0[2]))
